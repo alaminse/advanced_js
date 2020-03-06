55c1b67a566820df543959d0d7b37aded502e1d2
alaminse <contactalamin.se@gmail.com>
2020-03-06 18:09:33 +0600

    null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,25 @@
+//undefined variable
+let pakhi;
+console.log(pakhi);
+
+function add(num1, num2){
+    console.log(num1 + num2)
+    return
+}
+const result = add(13, 82);
+console.log(result)
+
+function add(num1, num2){
+    console.log(num1, num2)
+}
+const result = add(13);
+console.log(result)
+
+const pramik = {name:"noman", phone: 54545};
+console.log(pramik.id)
+
+let fun = undefined;
+console.log(fun)
+
+let ages = [2,4,5,6]
+console.log(ages[11])
